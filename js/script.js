5400b4f61bdc7784206418895f117f3ec65d28b3
salihsan <salih.kildis@gmail.com>
2021-06-12 20:11:11 +0300

    Duzeltmeler yapildi enter del calisiyor

--- js/script.js
+++ js/script.js
@@ -10,10 +10,10 @@ getData().then((data) => {
     cards.innerHTML += `
       <div class="sa mx-2 mt-5 " style="width: 25rem;">
 
-        <div class="card-img-top d-flex justify-content-center"><img src="${tvShow.show.image.medium}"></div>
+        <div class="card-img-top d-flex justify-content-center"><img class="img-fluid " src="${tvShow.show.image.medium}"></div>
         <div class="card-body">
-          <h4 class="card-title-name text-black bg-info px-3 py-2 rounded-3">${tvShow.show.name}</h4>
-          <h5 class="card-title text-black bg-danger px-3 py-2 rounded-3">${tvShow.show.name}</h5>
+          <h3 class="card-title-name  bg-primary bg-gradient text-white px-3 py-2 rounded-3">${tvShow.show.name}</h3>
+          <h3 class="card-title text-black text-black-50 px-3 py-2 rounded-3">IMDB : ${tvShow.show.externals.imdb}</h3>
           <p class="card-text ">${tvShow.show.summary}</p>
           <h5>Ulke:<span class="yil p-3 text-black-50 text-center mt-2">
           ${tvShow.show.network.country.name}</span>
@@ -22,7 +22,7 @@ getData().then((data) => {
           </h5>
           <h5>Film Turu:<span class="yil p-3 text-black-50 text-center mt-2">${tvShow.show.genres}</span>
           </h5>
-          <a href="#" class="btn btn-primary px-5">Detail</a>
+          <a href="${tvShow.show.url}" class="btn btn-primary px-5">Detail</a>
         </div>
     </div>
       `;
